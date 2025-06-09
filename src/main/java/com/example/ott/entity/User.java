821f2eefce442f39e622fbf4e1c036cab9a35119
HyunOwO <mogs223@gmail.com>
2025-06-09 17:41:54 +0900

    user Entity 속성 추가

--- src/main/java/com/example/ott/entity/User.java
+++ src/main/java/com/example/ott/entity/User.java
@@ -2,11 +2,14 @@ package com.example.ott.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
@@ -20,13 +23,20 @@ public class User {
     
     // TODO : UserCode 생성 기능 추가 필요
     @Id
-    private String userCode;
+    private String id;
 
-    @Column(nullable = false)
-    private String userName;
+    private String name;
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Setter
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole = UserRole.USER;
 
     @Builder.Default
     private Long mileage = 0L;
