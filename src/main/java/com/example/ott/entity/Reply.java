022204fc74f27c2f76bc7e2de26a85be3dd9ac78
SinSeonHo <loop63@naver.com>
2025-06-10 16:14:45 +0900

    대기

---
+++ src/main/java/com/example/ott/entity/Reply.java
@@ -0,0 +1,17 @@
+package com.example.ott.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Reply {
+    @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rid; // 댓글코드
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+}
