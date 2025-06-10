7ca24b0ae65f88ae885ad0a7574ac3af9d473fa1
geonho <0531a18@gmail.com>
2025-06-10 16:20:08 +0900

    commit test

---
+++ src/main/java/com/example/ott/entity/Reply.java
@@ -0,0 +1,66 @@
+package com.example.ott.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString(exclude = { "movie", "replyer" })
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "reply")
+
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Builder.Default
+    // 댓글의 추천수
+    private int recommend = 0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User replyer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mid")
+    private Movie movie;
+
+    // 대상 댓글의 rno
+    private Long ref;
+
+    // 대상 댓글의 유저 아이디
+    private Long mention;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    public void changeText(String text) {
+        this.text = text;
+    }
+}
