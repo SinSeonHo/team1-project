fd6ae34b67ae23ef5790457264824ee25a7f7a26
bree1206 <leeja45456@naver.com>
2025-06-11 17:22:18 +0900

    송민영 branch

--- src/main/java/com/example/ott/type/genretype.java
+++ src/main/java/com/example/ott/type/genretype.java
@@ -1,6 +1,10 @@
 package com.example.ott.type;
 
-public enum genretype {
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum Genretype {
     RPG,
 FPS,
 ADVENTURE,
@@ -18,5 +22,26 @@ THRILLER,
 SLICE_OF_LIFE,
 SCHOOL,
 SCI_FI,
-DOCUMENTARY
+DOCUMENTARY;
+
+ public static Set<Genretype> enumGenre(String pgenre) {
+        if (pgenre == null || pgenre.isBlank()) return Set.of();
+
+        return Arrays.stream(pgenre.split(",")).map(String::trim)
+        .map(String :: toUpperCase)
+        .filter(name -> {
+            try {
+                Genretype.valueOf(name);
+                return true;
+            } catch (IllegalArgumentException e) {
+                return false;
+            }
+        })
+        .map(Genretype :: valueOf)
+        .collect(Collectors.toSet());
+        
+    }
+
+
+
 }
