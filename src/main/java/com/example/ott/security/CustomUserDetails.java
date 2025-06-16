1543df896876d20713a2e6a603d882462d310a36
shin seung woo <ssw030209@gmail.com>
2025-06-16 16:19:11 +0900

    수정

--- src/main/java/com/example/ott/security/CustomUserDetails.java
+++ src/main/java/com/example/ott/security/CustomUserDetails.java
@@ -1,40 +1,41 @@
-package com.example.ott.security;
+// package com.example.ott.security;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.List;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.core.userdetails.UserDetails;
 
-import com.example.ott.entity.User;
+// import com.example.ott.entity.User;
 
-public class CustomUserDetails implements UserDetails {
+// public class CustomUserDetails implements UserDetails {
 
-    // User Entity 를 통째로 불러오기
-    private User user;
+// // User Entity 를 통째로 불러오기
+// private User user;
 
-    public CustomUserDetails(User user) {
-        this.user = user;
-    }
+// public CustomUserDetails(User user) {
+// this.user = user;
+// }
 
-    @Override
-    // 계정의 권한 목록을 리턴?
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<GrantedAuthority> authorities = this.user.getUserRole().getAuthorities();
+// @Override
+// // 계정의 권한 목록을 리턴?
+// public Collection<? extends GrantedAuthority> getAuthorities() {
+// List<GrantedAuthority> authorities =
+// this.user.getUserRole().getAuthorities();
 
-        return authorities;
-    }
+// return authorities;
+// }
 
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
+// @Override
+// public String getPassword() {
+// return user.getPassword();
+// }
 
-    @Override
-    public String getUsername() {
-        return user.getId();
-    }
+// @Override
+// public String getUsername() {
+// return user.getId();
+// }
 
-}
+// }
