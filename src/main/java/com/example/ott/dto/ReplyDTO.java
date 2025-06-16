236e7ddb464e2f30c37275f777789a48e60d8031
geonho <0531a18@gmail.com>
2025-06-16 12:18:01 +0900

    dto 주석, layout 수정

--- src/main/java/com/example/ott/dto/ReplyDTO.java
+++ src/main/java/com/example/ott/dto/ReplyDTO.java
@@ -12,10 +12,15 @@ import lombok.Data;
 public class ReplyDTO {
     private Long rno;
     private String text;
+    // 댓글의 추천수 지금은 안씀
     private int recommend;
+    // 댓글단 유저 아이디
     private String replyer;
+    // 영화 아이디
     private String mid;
+    // 대댓글이 달린 댓글의 id
     private Long ref;
+    // 대댓글이 달린 댓글의 유저명
     private String mention;
 
     private LocalDateTime createdDate;
