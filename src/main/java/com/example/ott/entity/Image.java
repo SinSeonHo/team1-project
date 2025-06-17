530c2e6e256049f15e9a5e4b431886759df1cd27
shin seung woo <ssw030209@gmail.com>
2025-06-17 18:16:50 +0900

    image 엔티티 수정

--- src/main/java/com/example/ott/entity/Image.java
+++ src/main/java/com/example/ott/entity/Image.java
@@ -1,28 +1,33 @@
 package com.example.ott.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
-@ToString(exclude = { "movie", "game", "webtoon" })
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
 @Getter
 @Setter
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = { "movie", "game", "user" }) // 순환참조 방지
 public class Image {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long inum; // 이미지 번호
     private String uuid; // 이미지 고유번호
-    private String imgName; // 이미지이름
+    private String imgName; // 이미지 이름
     private String path; // 경로
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
