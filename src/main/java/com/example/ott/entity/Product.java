7a6559ea271d8a5c56a86637ef1fd9684c69b0f1
SinSeonHo <loop63@naver.com>
2025-06-23 16:18:30 +0900

    이미지크롤링 db저장 자동화

--- src/main/java/com/example/ott/entity/Product.java
+++ src/main/java/com/example/ott/entity/Product.java
@@ -1,42 +1,42 @@
-package com.example.ott.entity;
+// package com.example.ott.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-import jakarta.persistence.*;
+// import jakarta.persistence.*;
 
-@Entity
-@Table(name = "product")
-public class Product {
+// @Entity
+// @Table(name = "product")
+// public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product-num")
-    private Long id;
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// @Column(name = "product-num")
+// private Long id;
 
-    @Column(name = "badge-name")
-    private String badgeName;
+// @Column(name = "badge-name")
+// private String badgeName;
 
-    @Column(name = "type") // 🔹 타입 컬럼 추가
-    private String type;
+// @Column(name = "type") // 🔹 타입 컬럼 추가
+// private String type;
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
-    private List<Image> images = new ArrayList<>();
+// @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+// private List<Image> images = new ArrayList<>();
 
-    // Getter & Setter
-    public Long getId() {
-        return id;
-    }
+// // Getter & Setter
+// public Long getId() {
+// return id;
+// }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+// public void setId(Long id) {
+// this.id = id;
+// }
 
-    public String getBadgeName() {
-        return badgeName;
-    }
+// public String getBadgeName() {
+// return badgeName;
+// }
 
-    public void setBadgeName(String badgeName) {
-        this.badgeName = badgeName;
-    }
-}
+// public void setBadgeName(String badgeName) {
+// this.badgeName = badgeName;
+// }
+// }
