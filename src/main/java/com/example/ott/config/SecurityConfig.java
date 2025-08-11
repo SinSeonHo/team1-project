865f1cd614618c002a13ac74e79b79eb80a7eb4a
HyunWoo <mogs223@gmail.com>
2025-08-11 10:33:03 +0900

    에러 코드 제거

--- src/main/java/com/example/ott/config/SecurityConfig.java
+++ src/main/java/com/example/ott/config/SecurityConfig.java
@@ -3,11 +3,10 @@ package com.example.ott.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-<<<<<<< HEAD
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-=======
->>>>>>> dev
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
